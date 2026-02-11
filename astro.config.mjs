6ead3f86248ccafd577bcc18c6f08c974c31c7e5
chengkoungmeng-hue <chengkoungmeng@gmail.com>
2026-02-11 19:43:23 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  // ✅ 新增這行：這是網站的身分證，SEO 必備！
+  site: 'https://ckmkh.com',
+  
   integrations: [tailwind()],
 });
